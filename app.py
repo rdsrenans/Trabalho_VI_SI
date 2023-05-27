e3493cff9e37547dfbd02211c003b39d0f0e06b5
Renan de Souza <renan.s@transpotech.com.br>
2023-05-27 18:05:12 -0300

    Add DataSet

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import reportlab.pdfgen.canvas as cv
 from reportlab.lib.units import mm
-from reportlab.pdfgen import canvas
 import numpy as np
 
-pdf = canvas.Canvas('Projeto Visualização da Informação.pdf')
+pdf = cv.Canvas('Projeto Visualização da Informação.pdf')
 df = pd.read_csv('cardio_train.csv', delimiter=';')
+
+df1 = df[['gender', 'cardio']]
+
+# plt.plot(df['gender'].unique())
+# plt.plot(df['cardio'].unique())
+plt.plot(df1)
+plt.xticks(np.arange(0, 2, 0.1))
+plt.grid(visible=bool, which='major', linestyle='-.')
+plt.show()
+
+
 
 # Head
 # pdf.drawString(20 * mm, 276 * mm, 'Nome: Renan Douglas de Souza')
@@ -19,22 +30,32 @@ df = pd.read_csv('cardio_train.csv', delimiter=';')
 # pdf.drawString(20 * mm, 236 * mm, 'Plot 1 - ')
 # pdf.setFont('Helvetica', 12)
 # pdf.drawString(36 * mm, 236 * mm, 'As 5 linguagens mais utilizadas pelos desenvolvedores de 2017 à 2022.')
-#
-# df = pd.DataFrame({'Anos': ['2017', '2018', '2019', '2020', '2021', '2022'],
-#                    'JavaScript': [65, 64, 69, 70, 69, 65],
-#                    'HTML/CSS': [60, 55, 61, 61, 60, 54],
-#                    'Python': [32, 41, 49, 55, 52, 53],
-#                    'SQL': [42, 47, 56, 56, 54, 49],
-#                    'Java': [47, 51, 50, 54, 49, 48]})
-#
-# for y in df:
-#     if y != 'Anos':
-#         plt.plot('Anos', y, label=y, data=df, linewidth=2)
-#
-# plt.ylabel('Uso em %')
-# plt.legend()
-# plt.grid()
-# plt.savefig('figure1')
+
+"""haveDisease = ('Saudável', 'Doente')
+data = df[['gender', 'cardio']]
+print(data)
+
+x = np.arange(len(haveDisease))
+width = 0.25
+multiplier = 0
+
+fig, ax = plt.subplots(layout='constrained')
+
+for attribute, measurement in data.items():
+    offset = width + multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+ax.set_ylabel('Quantidade de pessoas')
+ax.set_title('Quantidade de pessoas saudáveis x doentes')
+ax.set_xticks(x + width, haveDisease)
+ax.legend(loc='upper right', ncols=2)
+ax.set_ylim(0, 300)
+
+plt.show()"""
+
+# plt.savefig('figure1', dpi=300)
 # plt.cla()
 # plt.clf()
 #
@@ -47,39 +68,37 @@ df = pd.read_csv('cardio_train.csv', delimiter=';')
 # pdf.drawString(36 * mm, 168 * mm, 'O nível em qual eles julgam-se encaixarem.')
 #
 
-teste = df[['cardio', 'gender']]
-
-fig, ax = plt.subplots()
-
-size = 0.3
-vals = np.array([df['cardio'], df['gender']])
-print(vals)
-
-cmap = plt.colormaps["tab20c"]
-outer_colors = cmap(np.arange(3)*4)
-inner_colors = cmap([1, 2, 5, 6, 8, 10])
-
-ax.pie(vals.sum(axis=1), radius=1, colors=outer_colors,
-       wedgeprops=dict(width=size, edgecolor='w'))
-
-ax.pie(vals.flatten(), radius=1-size, colors=inner_colors,
-       wedgeprops=dict(width=size, edgecolor='w'))
-
-
-ax.set(aspect="equal", title='Pie plot with `ax.pie`')
-plt.show()
-
-labels = ['Homem', 'Mulher']
-sizes = df['gender'].unique()
-
-colors = ['#913f33', '#ff705f']
-patches, texts, autotexts = plt.pie(sizes, colors=colors, autopct='%1.0f%%', startangle=90, pctdistance=1.15)
-
-plt.legend(patches, labels, loc="lower right")
-plt.axis('equal')
-plt.savefig('figure2')
-plt.cla()
-plt.clf()
+# fig, ax = plt.subplots()
+#
+# size = 0.3
+# vals = np.array([df['cardio'], df['gender']])
+# print(vals)
+#
+# cmap = plt.colormaps["tab20c"]
+# outer_colors = cmap(np.arange(3)*4)
+# inner_colors = cmap([1, 2, 5, 6, 8, 10])
+#
+# ax.pie(vals.sum(axis=1), radius=1, colors=outer_colors,
+#        wedgeprops=dict(width=size, edgecolor='w'))
+#
+# ax.pie(vals.flatten(), radius=1-size, colors=inner_colors,
+#        wedgeprops=dict(width=size, edgecolor='w'))
+#
+#
+# ax.set(aspect="equal", title='Pie plot with `ax.pie`')
+# plt.show()
+#
+# labels = ['Homem', 'Mulher']
+# sizes = df['gender'].unique()
+#
+# colors = ['#913f33', '#ff705f']
+# patches, texts, autotexts = plt.pie(sizes, colors=colors, autopct='%1.0f%%', startangle=90, pctdistance=1.15)
+#
+# plt.legend(patches, labels, loc="lower right")
+# plt.axis('equal')
+# plt.savefig('figure2', dpi=300)
+# plt.cla()
+# plt.clf()
 #
 # pdf.drawImage('figure2.png', 20 * mm, 110 * mm, width=212, height=159)
 #
@@ -98,17 +117,11 @@ plt.clf()
 #     bar_container, fmt=lambda x: '{}%'.format(x)
 # )
 #
-# plt.savefig('figure3')
+# plt.savefig('figure3', dpi=300)
 #
 # pdf.drawImage('figure3.png', 20 * mm, 42 * mm, width=212, height=159)
 #
 # pdf.save()
-
-
-
-
-
-
 
 
 """################################## Antes ####################################################
